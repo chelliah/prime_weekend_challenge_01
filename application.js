ef5870e86c82bcb98cb46350c19a79f95481d0e1
Aron <athomas5@macalester.edu>
2015-10-18 14:37:29 -0500

    Add comments and reformat delete button

--- application.js
+++ application.js
@@ -1,5 +1,7 @@
-var employeeLog = [];
+//initialize an empty 
+var employeeLog = []; 
 
+//adds new employee info to the DOM
 function appendDom(employee){
 	$("#employees").append('<div class="employee" id= "employee' + employee.idNo + '""></div>');
 	var $el = $("#employees").children().last();
@@ -7,20 +9,17 @@ function appendDom(employee){
 	var string = employee.firstName + " " + employee.lastName + ": " + employee.idNo + ", "  + employee.jobTitle + ", "  + employee.yearlySalary; //formats object into new string
 
 	$el.append("<p>" + string + "</p>") 
-	// $el.append("<p>" + employee.firstName + "</p>");
-	// $el.append("<p>" + employee.lastName + "</p>");
-	// $el.append("<p>" + employee.idNo + "</p>");
-	// $el.append("<p>" + employee.jobTitle + "</p>");
-	// $el.append("<p>" + employee.yearlySalary + "</p>");
 }
 
+//calls the combile salareies method and updates this information to the DOM
 function calculateNewSalary(){ //
 	var totalSalary = compileSalaries(employeeLog);
 	$("#salaryMessage").text("The monthly cost of employee salaries is: " + totalSalary + " dollars");
 	
 }
 
-function compileSalaries(log){ //combile salaries loops through all of the employees in the array, totals the income values and returns the monthly average rounded to the nearest dollar
+//combile salaries loops through all of the employees in the array, totals the income values and returns the monthly average rounded to the nearest dollar
+function compileSalaries(log){ 
 	var totalSalaries = 0;
 	for(employee in log){
 		var salary = parseInt(removeNonNumberic(log[employee].yearlySalary));
@@ -29,21 +28,22 @@ function compileSalaries(log){ //combile salaries loops through all of the emplo
 	return Math.round(totalSalaries/12);
 };
 
-function removeNonNumberic(str){ //removes non numeric values from an input string
+//removes non numeric values from an input string
+function removeNonNumberic(str){ 
 	var numericString = str.replace(/[^0-9]/g, '');
 	return numericString;
 }
 
+//asks user to enter ID, uses this value to search through employee log for the correct employee, and then deletes that employee from the log
 function deleteEmployee() {
 	var personID = prompt("Please enter the ID number of the employee you wish to delete");
 	var deletedEmployee;
 	for(employee in employeeLog){
 		if(employeeLog[employee].idNo==personID){
 			deletedEmployee = employeeLog[employee];
-			employeeLog.splice(employee,1);
+			employeeLog.splice(employee,1); //removes that employee from the array
 		}
 	}
-	// var deletedEmployee = employeeLog.pop();
 	var id = deletedEmployee.idNo;
 	var employee = '#employee' + id;
 	$(employee).remove();
@@ -56,20 +56,23 @@ $(document).ready(function(){//can put entireity of code in a function
 		var values = {};//create object to store information from event
 
 		$.each($("#employeeInfo").serializeArray(), /* creates an array of objects */
-			function(i, field){ /* i is how we are steppign through the array, field is the information*/
+			function(i, field){ /* i is how we are stepping through the array, field is the information*/
 				values[field.name] = field.value;
 		});//grab each thing off of a form
 		$("#employeeInfo").find("input[type=text]").val("");
 		appendDom(values);
 		employeeLog.push(values);
+		calculateNewSalary();
 		console.log(values);
 	});
 
-	$('#calculateSalary').on('click', calculateNewSalary);
-
 	$('#delete').on('click',function(){
-		deleteEmployee();
-		calculateNewSalary();
+		if(employeeLog.length==0){
+			alert("Employee log is empty! Nothing to delete!");
+		}else{
+			deleteEmployee();
+			calculateNewSalary();
+		}
 	})
 });
 
